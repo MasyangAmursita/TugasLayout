9913c258b4647e4dfe5b1c943cc11559344ca673
MasyangAmursita <37802428+MasyangAmursita@users.noreply.github.com>
2018-03-26 20:39:58 +0800

    Add files via upload

---
+++ AppWeather1615051009/App.js
@@ -0,0 +1,20 @@
+//import React from 'react';
+//import BelajarLayout from './src/comp/BelajarLayout';
+
+//export default class App extends React.Component {
+  //render() {
+    //return (
+    //  <BelajarLayout />
+  //  );
+//  }
+//}
+import React from 'react';
+import Tugas2 from './src/comp/Tugas2';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Tugas2 />
+    );
+  }
+}
---
+++ AppWeather1615051009/src/comp/Tugas2.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import {
+  Button,
+  Text, TextInput,
+  View
+} from 'react-native';
+
+export default class HitungVolumeBalok extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      panjang: 0,
+      lebar: 0,
+      tinggi: 0,
+      volume: 0
+    };
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, backgroundColor: '#bbdefb' }}>
+
+        <View style={{ backgroundColor: '#2196f3' }}>
+           <Text style={{ padding: 10, fontSize: 20, color: 'white', textAlign: 'center' }} >
+            Menghitung Volume Balok
+          </Text>
+         </View>
+
+        <View style={{ margin: 20, padding: 10, backgroundColor: '#e3f2fd' }}>
+            <TextInput style={{ height: 90 }}
+              placeholder="Masukan Kota"
+              keyboardType='Text'
+            />
+
+            <Button
+              onPress={() => this.setState({
+                volume: (this.state.panjang * this.state.lebar * this.state.tinggi)
+              })}
+              title="Submith"
+              accessibilityLabel="Klik untuk menghitung"
+            />
+       </View>
+
+        <View style={{ margin: 20, backgroundColor: '#90caf9' }}>
+          <Text style={{ padding: 10, fontSize: 20 }} >
+
+          </Text>
+         </View>
+
+   </View>
+
+    );
+  }
+
+}
